077af3135ab2f77e7e4526217c124ae482761772
Your Name <you@example.com>
2025-08-27 23:04:33 -0700

    js Events

---
+++ script2-event.js
@@ -0,0 +1,30 @@
+function event_click()
+{
+    alert("Form Submitted Successfully");
+    document.write("Do you want to save password?");
+}
+function event_mouseover()
+{
+    document.getElementsByName("username")[0].style.backgroundColor = "lightblue";
+    document.getElementsByName("password")[0].style.color = "black";
+    document.getElementsByName("password")[0].style.backgroundColor = "lightblue";
+    document.getElementsByName("username")[0].style.borderRadius = "10px";
+    document.getElementsByName("password")[0].style.borderRadius = "10px";
+}
+// javascript event handling example
+// onclick event example
+// onmouseover event example    
+// onmouseout event example
+// onkeydown event example
+// onkeyup event example
+// onload event example
+// onunload event example
+// onfocus event example
+// onblur event example
+// onchange event example
+// onsubmit event example
+// onreset event example
+// onselect event example
+// oncontextmenu event example
+// ondrag event example
+// and many more events...
