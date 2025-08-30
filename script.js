fc28eb7d8cdcea21b337e80bdee8656cd4f07299
Ghani Hussaini <157278208+GhaniHussaini@users.noreply.github.com>
2025-08-31 00:05:13 +0430

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+// script.js
+function hideSidebar() {
+    document.querySelector('.sidebar').setAttribute('class', 'hide-sidebar');
+}   
+function showSidebar() {
+    document.querySelector('.hide-sidebar').setAttribute('class', 'sidebar');
+}   
